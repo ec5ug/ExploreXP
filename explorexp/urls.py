88a286f6cdf0a44416ff2ab5c52c0d87d385b90d
Brooke Hewitt <112017131+brookeahewitt@users.noreply.github.com>
2024-02-24 12:37:27 -0500

    Login and logout working successfully

--- explorexp/urls.py
+++ explorexp/urls.py
@@ -22,7 +22,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('allauth.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html'), name="index"),
     path('accounts/', include('allauth.urls')),
-    path('logout/', LogoutView.as_view())
+    path('logout/', views.logout_view),
+    path('home', views.home, name="home")
 ]
