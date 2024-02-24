1a29279cf3e0c0aab2614403a5244fe38172fbf9
ec5ug <ec5ug@virginia.edu>
2024-02-24 11:48:27 -0500

    logout supposedly works

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -6,8 +6,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('app/', include('auth_app.urls')),
-
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name='index.html')),
     #path('accounts/', include('allauth.urls')),
     #path('logout', LogoutView.as_view())
 ]
