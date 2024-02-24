b5115fe4f5faf92d85d84738b3167822474f40fb
ec5ug <ec5ug@virginia.edu>
2024-02-24 14:25:08 -0500

    edited Category model

--- explorexp/models.py
+++ explorexp/models.py
@@ -19,17 +19,17 @@ class Place(models.Model):
         self.name_slug = slugify(self.name)
         super(Place, self).save(*args, **kwargs)
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 class Challenge(models.Model):
     name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
     place = models.ForeignKey(Place, related_name='challenges', on_delete=models.CASCADE)
 
 class Post(models.Model):
     place = models.ForeignKey(Place, related_name='posts', on_delete=models.CASCADE)
     challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
-
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-    def __str__(self):
-        return self.name
