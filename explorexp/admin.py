11c8917b839ce9bd32e4faa741551236b1c9fe45
ec5ug <ec5ug@virginia.edu>
2024-02-24 13:16:03 -0500

    registered models

---
+++ explorexp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Place, Challenge, Post, Category
+
+admin.site.register(Place)
+admin.site.register(Challenge)
+admin.site.register(Post)
+admin.site.register(Category)
