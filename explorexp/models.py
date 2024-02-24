2fc17f96d2aea088daa2028b1d01595c29b79370
ec5ug <ec5ug@virginia.edu>
2024-02-24 16:57:23 -0500

    created synthetic data

--- explorexp/models.py
+++ explorexp/models.py
@@ -29,20 +29,15 @@ class Challenge(models.Model):
     description = models.CharField(max_length=500)
     place = models.ForeignKey(Place, related_name='challenges', on_delete=models.CASCADE)
     points = models.IntegerField(default=0)
-
+    def __str__(self):
+        return f"{self.name} | {self.place}"
 
 class Post(models.Model):
-    place = models.ForeignKey(Place, related_name='posts', on_delete=models.CASCADE)
     challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
     def __str__(self):
-        return self.name
-
-
+        return f"{self.user} | {self.challenge}"
 class User(models.Model):
     user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
     challenges_completed = models.ManyToManyField(Challenge, related_name='challenges_completed', blank=True)
