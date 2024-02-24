f2f3becbadaa96b7896a44da23387efc9d4b9a81
awhitehouse1 <112509418+awhitehouse1@users.noreply.github.com>
2024-02-24 10:28:25 -0500

    updated settings

--- explorexp/settings.py
+++ explorexp/settings.py
@@ -116,3 +116,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+API_KEY = os.getenv('GOOGLE_SSO_CLIENT_ID')
--- explorexp/views.py
+++ explorexp/views.py
@@ -1,4 +1,5 @@
 # homepage/views.py
+
 from django.shortcuts import render
 
 def home(request):
